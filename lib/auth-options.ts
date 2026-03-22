9f4f6d518f99fc946b23982c85bf374b53eb27b2
umidjon11111 <umidjongafforov844@gmail.com>
2026-03-22 20:57:58 +0500

    nextauth

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -46,65 +46,8 @@ async function oauthLoginOrRegister(
   }
 }
 
-const useSecureCookies =
-  process.env.NEXTAUTH_URL?.startsWith("https://") ?? false;
 
 export const authOptions: NextAuthOptions = {
-  cookies: {
-    sessionToken: {
-      name: useSecureCookies
-        ? "__Secure-next-auth.session-token"
-        : "next-auth.session-token",
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: useSecureCookies,
-      },
-    },
-    callbackUrl: {
-      name: useSecureCookies
-        ? "__Secure-next-auth.callback-url"
-        : "next-auth.callback-url",
-      options: { sameSite: "lax", path: "/", secure: useSecureCookies },
-    },
-    csrfToken: {
-      name: useSecureCookies
-        ? "__Host-next-auth.csrf-token"
-        : "next-auth.csrf-token",
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: useSecureCookies,
-      },
-    },
-    state: {
-      name: useSecureCookies
-        ? "__Secure-next-auth.state"
-        : "next-auth.state",
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: useSecureCookies,
-        maxAge: 900,
-      },
-    },
-    pkceCodeVerifier: {
-      name: useSecureCookies
-        ? "__Secure-next-auth.pkce.code_verifier"
-        : "next-auth.pkce.code_verifier",
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: useSecureCookies,
-        maxAge: 900,
-      },
-    },
-  },
-
   providers: [
     CredentialsProvider({
       name: "Credentials",
